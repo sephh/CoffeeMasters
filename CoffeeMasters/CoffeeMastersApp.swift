2e78188ef6d7ccdac5ed6f56f36aca8e114f3d8a
Azure Pipelines <localiza@loclaiza.com>
2023-02-06 06:48:46 -0300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Greg on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
